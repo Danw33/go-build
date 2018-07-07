66ef56141ba1a87eefc3771dba6e08923193e7cd
Daniel Wilson <hello@danw.io>
2018-07-07 11:14:35 +0100

    Robustness and logging in the extension/plugin loader 🤜🤛 🔌

--- src/extension.go
+++ src/extension.go
@@ -27,17 +27,17 @@ SOFTWARE.
 package main
 
 import (
-	"plugin"
 	"github.com/op/go-logging"
+	"plugin"
 )
 
 // BuildPlugin is an interface for go-build plugins
 type BuildPlugin interface {
 	// pluginInit 0. Plugin Initialiser, called on load of plugin file
-	pluginInit(*logging.Logger, configuration, string) error
+	pluginInit(*logging.Logger, *configuration, string) error
 
 	// postLoadPlugins 1. First hook, after plugins are loaded
-	postLoadPlugins([]*plugin.Plugin, configuration)
+	postLoadPlugins(*[]BuildPlugin, *configuration)
 
 	// preProcessProjects 2. Before processing all projects
 	preProcessProjects()
@@ -68,6 +68,13 @@ var loadedPlugins []*plugin.Plugin
 var buildPlugins []BuildPlugin
 
 func loadPlugins(config *configuration) {
+
+	// See if the config defines any plugins
+	if len(config.Plugins) == 0 {
+		log.Info("No plugins configured, bypassing plugin loader.")
+		return
+	}
+
 	// Load in plugin files
 	for _, pFile := range config.Plugins {
 		if plug, err := plugin.Open(pFile); err == nil {
@@ -78,18 +85,45 @@ func loadPlugins(config *configuration) {
 		}
 	}
 
-	// Locate buildPlugin symbols and test
+	// See if we loaded any plugins from the disk
+	if len(loadedPlugins) == 0 {
+		log.Info("No configured plugins could be loaded.")
+		return
+	}
+
+	// For each plugin we loaded, Locate BuildPlugin symbol and check it
 	for _, p := range loadedPlugins {
+		// Lookup the symbol
 		symPlug, err := p.Lookup("BuildPlugin")
 		if err != nil {
 			log.Errorf("Plugin exports no BuildPlugin symbol: %v", err)
 			continue
 		}
+
+		// Check it's compatible with our definition of the BuildPlugin interface
 		bp, ok := symPlug.(BuildPlugin)
 		if !ok {
 			log.Errorf("Build Plugin is not an BuildPlugin interface type")
 			continue
 		}
+
+		// Call the pluginInit for the current plugin
+		initErr := bp.pluginInit(log, config, Version)
+		if initErr != nil {
+			log.Errorf("Plugin loaded but failed to initialise: %v", initErr)
+			continue
+		}
+
+		// Add it to the array of initialised plugins
 		buildPlugins = append(buildPlugins, bp)
+	}
+
+	// Log plugin status now loading is completed
+	log.Debugf("Plugin Loader: %d found in config, %d loaded from filesystem, %d compatible and initialised.", len(config.Plugins), len(loadedPlugins), len(buildPlugins))
+	log.Infof("Initialised %d plugins successfully", len(buildPlugins))
+
+	// Call the postLoadPlugins method for each loaded plugin
+	for _, lp := range buildPlugins {
+		lp.postLoadPlugins(&buildPlugins, config)
 	}
 }
